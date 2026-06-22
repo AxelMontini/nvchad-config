020ac65dd6f1b62ab3aa18918f5dcaa37e3fee1b
Axel Montini <axel.montini@proton.me>
2026-06-22 18:18:27 +0200

    add trouble: error diagnostics

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,41 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+
+local trouble_keys = {
+  {
+    "<leader>xx",
+    "<cmd>Trouble diagnostics toggle<cr>",
+    { desc = "Diagnostics (Trouble)" },
+  },
+  {
+    "<leader>xX",
+    "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+    { desc = "Buffer Diagnostics (Trouble)" },
+  },
+  {
+    "<leader>cs",
+    "<cmd>Trouble symbols toggle focus=false<cr>",
+    { desc = "Symbols (Trouble)" },
+  },
+  {
+    "<leader>cl",
+    "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+    { desc = "LSP Definitions / references / ... (Trouble)" },
+  },
+  {
+    "<leader>xL",
+    "<cmd>Trouble loclist toggle<cr>",
+    { desc = "Location List (Trouble)" },
+  },
+  {
+    "<leader>xQ",
+    "<cmd>Trouble qflist toggle<cr>",
+    { desc = "Quickfix List (Trouble)" },
+  },
+}
+
+for m in trouble_keys do
+  map("n", m[1], m[2], m[3])
+end
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -20,6 +20,44 @@ return {
     config = function()
       require "configs.rainbow-delimiters"
     end,
+  },
+
+  {
+    "folke/trouble.nvim",
+    opts = require "configs.trouble", -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
   }
 
   -- test new blink
